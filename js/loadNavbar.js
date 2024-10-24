99b8ff08660653c6353cff2a16f750598fdd32e2
visualmud <visualmud@gmail.com>
2024-10-24 17:09:39 +0200

    Update loadNavbar.js

--- js/loadNavbar.js
+++ js/loadNavbar.js
@@ -1,1 +1,20 @@
+function loadNavbar() {
+    fetch('nav.html')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.text();
+        })
+        .then(data => {
+            const navbarPlaceholder = document.getElementById('navbar-placeholder');
+            if (navbarPlaceholder) {
+                navbarPlaceholder.innerHTML = data;
+            } else {
+                console.error('Navbar placeholder element not found.');
+            }
+        })
+        .catch(error => console.error('Error loading navigation:', error));
+}
 
+document.addEventListener('DOMContentLoaded', loadNavbar);
